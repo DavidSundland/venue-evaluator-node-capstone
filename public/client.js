dfbd95e2ec1e39ac3f8f6fe4a803a260c5c64f23
D Sundland <info@districtconcerts.com>
2018-01-31 15:05:14 -0500

    added function to generate venue list

--- public/client.js
+++ public/client.js
@@ -83,6 +83,85 @@ function raiseCurtain() {
 }
 
 
+function listVenues() {
+    let testVenueList = [{
+        "Venue_Name": "701 Restaurant",
+        "Primary_Genre": "Potpourri",
+        "Website": "http://701restaurant.com/about/live-jazz/",
+        "Google_Map_URL": "https://goo.gl/maps/seWMAXmPyKP2",
+        "Latitude": 38.894292,
+        "Longitude": -77.022442,
+        "Full_Address": "701 Pennsylvania Ave. NW, Washington, DC 20004",
+        "Street_Address": "701 Pennsylvania Ave. NW",
+        "City": "Washington",
+        "State": "DC",
+        "Zip": 20004,
+        "Description": "701 offers the warmth and charm of a private club and a picturesque setting, providing guests with a truly memorable dining experience. Menus reflect an inventive approach to Contemporary Continental Cuisine. 701 also offers a bar with a vast selection of culinary treats and signature cocktails, and a recently renovated 'back lounge.' During the warmer months, the patio provides a pleasant view of the Navy Memorial's cascading fountains and the National Archives. Live piano is provided in the evening, Thursday through Saturday.",
+        "Image_URL": "https://github.com/DavidSundland/venue-evaluator-node-capstone/blob/master/public/images/701.jpg?raw=true"
+}, {
+        "Venue_Name": "9:30 Club",
+        "Primary_Genre": "Rock & Pop",
+        "Website": "http://www.930.com/",
+        "Google_Map_URL": "https://goo.gl/maps/EbAXTg6iEvE2",
+        "Latitude": 38.9179969,
+        "Longitude": -77.0258807,
+        "Full_Address": "815 V St. NW, Washington, DC 20001",
+        "Street_Address": "815 V St. NW",
+        "City": "Washington",
+        "State": "DC",
+        "Zip": 20001,
+        "Description": "9:30 Club is a standing-only venue with a capacity of 1200 people. It is a state-of-the-art concert space which presents top-name rock, punk, hip-hop and country acts nightly. It has won multiple Billboard Touring Awards and Pollstar 'Nightclub of the Year' honors, and was rated as the #1 Big Room in America by Rolling Stone Magazine Online. It has long been the District's premier venue for nationally known touring acts.",
+        "Image_URL": "https://github.com/DavidSundland/venue-evaluator-node-capstone/blob/master/public/images/930.jpg?raw=true"
+}, {
+        "Venue_Name": "Museum of American Art",
+        "Primary_Genre": "Classical",
+        "Website": "http://www.si.edu/Events/Calendar/?trumbaEmbed=filter1%3D_%26-index%26filterfield1%3D11153#/?i=2",
+        "Google_Map_URL": "https://goo.gl/maps/LqZXy3Q9DxJ2",
+        "Latitude": 38.8978609,
+        "Longitude": -77.0251306,
+        "Full_Address": "800 G St. NW, Washington, DC 20001",
+        "Street_Address": "800 G St. NW",
+        "City": "Washington",
+        "State": "DC",
+        "Zip": 20001,
+        "Description": "The Smithsonian American Art Museum and National Portrait Gallery share the Nan Tucker McEvoy Auditorium, a 346-seat facility located on the building's lower level, and the Kogod Courtyard, a covered space in the center of the building. Monthly concerts are also held in the American Art Museum's Luce Center.  In addition to music, public programming includes lectures, films, theater and dance performances.",
+        "Image_URL": "https://github.com/DavidSundland/venue-evaluator-node-capstone/blob/master/public/images/american-art.jpg?raw=true"
+}, {
+        "Venue_Name": "American University - Greenberg",
+        "Primary_Genre": "Classical",
+        "Website": "http://american.tix.com",
+        "Google_Map_URL": "https://goo.gl/maps/QFPcdRVHZg62",
+        "Latitude": 38.9434072,
+        "Longitude": -77.0798043,
+        "Full_Address": "4200 Wisconsin Ave. NW, Washington, DC 20016",
+        "Street_Address": "4200 Wisconsin Ave. NW",
+        "City": "Washington",
+        "State": "DC",
+        "Zip": 20016,
+        "Description": "American University operates the off-campus Greenberg Theatre, which principally showcases the university, but which also features guest artists on occasion. Its mission is to provide American University and the local community a venue for live performances in music, theatre and dance. It is the primary performance space for AU's Performing Arts Department.",
+        "Image_URL": "https://github.com/DavidSundland/venue-evaluator-node-capstone/blob/master/public/images/greenberg.jpg?raw=true"
+}];
+    $("#listBox").text(""); /* clear existing text, if any */
+    for (let x = 0; x < testVenueList.length; x++) {
+        testVenueList[x].Description.replace(/\s+/g, ' '); /* eliminate extra spaces, if any */
+        let wordList = testVenueList[x].Description.split(" ");
+        let maxWords = 30;
+        let description = "";
+        if (wordList.length > maxWords) {
+            for (y = 0; y < maxWords; y++) {
+                description += wordList[y] + " ";
+            }
+            description += "...";
+        } else {
+            description = testVenueList[x].Description;
+        }
+        $("#listBox").append(`<p class="oneVenue"><a href="${testVenueList[x].Website}" class="venueName">${testVenueList[x].Venue_Name}</a> - <span class="address">${testVenueList[x].Street_Address}</span><br><span class="description">${description}</span><button>More Info</button></p>`);
+    }
+}
+
+
+
+
 //function initVenueMap() {
 //    console.log("In initVenueMap");
 //    var venueMap = new google.maps.Map(document.getElementById('venueMap'), {
@@ -110,3 +189,4 @@ function raiseCurtain() {
 $(clickVenue);
 $(clickClose);
 $(raiseCurtain);
+$(listVenues);
